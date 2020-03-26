77d94abcd892f0ee1fd075bf676e377959ff17ac
Nora van den Heuvel <noravdheuvel@gmail.com>
2020-03-26 16:55:55 +0000

    added styling

--- src/interface.js
+++ src/interface.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
-	
+	$("#hiddenRow-0").hide();
 	$("#hiddenRow-1").hide();
 	$("#hiddenRow-2").hide();
 	$("#hiddenRow-3").hide();
 	$("#hiddenRow-4").hide();
+	$("#hiddenRow-5").hide();
+	$("#hiddenRow-6").hide();
+	$("#hiddenRow-7").hide();
+	$("#hiddenRow-8").hide();
+	$("#hiddenRow-9").hide();
+	$("#hiddenRow-10").hide();
 	var office = new Office();
 
   $('#add-room').submit(function(event) {
@@ -12,6 +18,7 @@ $(document).ready(function() {
     office.add(room)
     $('#room-name').val('');
 		$(`.room-item-${office.list().length} .value`).html(`${room.name}`);
+		$("#hiddenRow-0").show();
 		$(`#hiddenRow-${office.list().length}`).toggle();
 	})
 
--- src/meetingRoom.js
+++ src/meetingRoom.js
@@ -6,12 +6,18 @@ class MeetingRoom{
 
   enter() {
     if(this.isAvailable() === false){
+      alert('Sorry, this meeting room is not available')
       throw new Error('cannot enter unavailable room');
+      
     }
     this.available = false;
   }
 
   leave() {
+    if(this.isAvailable() === true){
+      alert('Sorry, you cannot leave an empty room')
+      
+    }
     this.available = true;
   }
 
